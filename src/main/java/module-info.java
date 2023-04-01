30a9dc2a6dedd0efd75b01beaac79c8e9667f672
Aleksandrone1 <alpeeffalex@yandex.ru>
2023-04-02 00:45:34 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module AlpeevAlexISP201S {
+
+    requires java.desktop;
+    requires java.sql;
+    requires barcode4j;
+    requires barbecue;
+    requires javafx.fxml;
+    requires  javafx.graphics;
+    requires javafx.controls;
+    requires javafx.swing;
+    requires mssql.jdbc;
+    requires com.google.zxing;
+    requires  itextpdf;
+    requires java.mail;
+    opens sample;
+}
